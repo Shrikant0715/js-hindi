a6fa03e4217b15e13089be2c6eb5e98128bbfe53
Shrikant0715 <shrikantgaikwad0715@gmail.com>
2024-06-28 06:43:18 +0000

    conversion operation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = 33
+
+console.log(typesof score);
+console.log(typesof(score));
+
+let valueInNumber = Number(score)
+console.log(typesof valueInNumber);
+console.log(valueInNumber);
+
+// '33 ==> 33
+//"33abc" ==> NaN
+//true =>1; false=>0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+
+
+ 
